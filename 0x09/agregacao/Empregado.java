9153378a7e39f454148c163bcf19da5b8c52a2dc
jcavalaro <jcavalaro@ciandt.com>
2022-05-18 11:59:32 -0300

    task0

---
+++ 0x09/agregacao/Empregado.java
@@ -0,0 +1,37 @@
+public class Empregado {
+
+    private String nome;
+    private int codigo;
+    private Endereco endereco;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public int getCodigo() {
+        return codigo;
+    }
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    public Endereco getEndereco() {
+        return endereco;
+    }
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+    public Empregado(String nome, int codigo, Endereco endereco) {
+        this.nome = nome;
+        this.codigo = codigo;
+        this.endereco = endereco;
+    }
+    
+    public void apresentar(){
+        System.out.println("Codigo: "+ this.getCodigo());
+        System.out.println("Nome: " + this.getNome());
+        System.out.println("Bairro: "+ this.getEndereco().getBairro());
+        System.out.println("Cidade: "+ this.getEndereco().getCidade());
+        System.out.println("Pais: "+ this.getEndereco().getPais());
+    }
+}
