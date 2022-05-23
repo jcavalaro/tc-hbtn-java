6a28dd943f61657deef063e21900dcf8011c661f
jcavalaro <jcavalaro@ciandt.com>
2022-05-23 12:09:19 -0300

    task0

---
+++ 0x01/numeros/Numeros0a99.java
@@ -0,0 +1,16 @@
+public class Numeros0a99 {
+    
+	public static void main(String[] args) {
+		int contador=99;
+		for (int i=0; i<=contador; i++) {
+			if(i<99) {
+				System.out.print(i +", ");
+			}else {
+				System.out.println(i);	
+			}
+			
+		}
+		
+	}
+
+}
