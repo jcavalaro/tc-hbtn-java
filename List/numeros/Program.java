616e216c7ccfc3c26ccf607791f282f7296cd5cf
jcavalaro <jcavalaro@ciandt.com>
2022-05-30 14:56:27 -0300

    add  throws Exception

--- List/numeros/Program.java
+++ List/numeros/Program.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Program {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         List<Integer> numeros = new ArrayList<Integer>();
 
         ManipularArrayNumeros.adicionarNumero(numeros, 1);
