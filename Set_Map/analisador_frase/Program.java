45911567a486b0d004580f767cf392c62f3c9902
jcavalaro <jcavalaro@ciandt.com>
2022-06-02 09:13:12 -0300

    add program

--- Set_Map/analisador_frase/Program.java
+++ Set_Map/analisador_frase/Program.java
@@ -1,17 +1,19 @@
-package duplicados;
-
 import java.util.*;
 
 public class Program {
 	public static void main(String[] args) {
-        TreeSet<Integer> duplicados1 = NumerosDuplicados.buscar(new int[] {1, 2, 3, 1, 3, 2, 5, 8, 9, 6, 7, 6, 1, 2});
-        TreeSet<Integer> duplicados2 = NumerosDuplicados.buscar(new int[] {6, 17, 19, 11, 4, 18, 5, 20, 22, 5, 1, 19, 25, 14, 7, 8, 18, 16, 1, 12, 4, 18, 15, 27, 2, 24, 6, 23, 3, 2});
-        TreeSet<Integer> duplicados3 = NumerosDuplicados.buscar(new int[] {9, 15, 6, 11, 1, 14, 16, 9, 5, 4, 16, 13, 9, 17, 5, 7, 16, 3, 7, 15});
-        TreeSet<Integer> duplicados4 = NumerosDuplicados.buscar(new int[] {2, 2, 18, 16, 4, 22, 23, 18, 5, 13, 21, 7, 9, 12, 2, 17, 14, 12, 14, 20, 10, 20, 13, 18, 23, 21, 19, 8, 11, 13});
+        TreeMap<String, Integer> contagemFrase1 = AnalisadorFrase.contagemPalavras("Tres pratos de trigo para tres tigres tristes");
+        TreeMap<String, Integer> contagemFrase2 = AnalisadorFrase.contagemPalavras("O que eh que Caca quer? Caca quer caqui. Qual caqui que Caca quer? Caca quer qualquer caqui.");
+        TreeMap<String, Integer> contagemFrase3 = AnalisadorFrase.contagemPalavras("Larga a tia, largatixa! Lagartixa, larga a tia!");
+        TreeMap<String, Integer> contagemFrase4 = AnalisadorFrase.contagemPalavras("Maria-mole eh molenga, se nao eh molenga, nao eh maria-mole. Eh coisa malemolente, nem mala, nem mola, nem Maria, nem mole.");
+        TreeMap<String, Integer> contagemFrase5 = AnalisadorFrase.contagemPalavras("Pilha de palha e telha velha. Palha na pilha e velha telha. Pilha de telha e palha velha.");
+        TreeMap<String, Integer> contagemFrase6 = AnalisadorFrase.contagemPalavras("Embaixo da pia tem um pinto que pia, quanto mais a pia pinga mais o pinto pia!");
 
-        System.out.println(duplicados1);
-        System.out.println(duplicados2);
-        System.out.println(duplicados3);
-        System.out.println(duplicados4);
+        System.out.println(contagemFrase1);
+        System.out.println(contagemFrase2);
+        System.out.println(contagemFrase3);
+        System.out.println(contagemFrase4);
+        System.out.println(contagemFrase5);
+        System.out.println(contagemFrase6);
     }    
 }
