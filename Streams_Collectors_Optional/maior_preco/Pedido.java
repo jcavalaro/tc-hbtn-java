ead93cd65dc88071f7ef5558cb9d65701e9c672a
jcavalaro <jcavalaro@ciandt.com>
2022-06-13 14:54:23 -0300

    task obter maior num

--- Streams_Collectors_Optional/maior_preco/Pedido.java
+++ Streams_Collectors_Optional/maior_preco/Pedido.java
@@ -13,6 +13,32 @@ public class Pedido {
 		this.cliente = cliente;
 	}
 
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+
+
 
 
 }
