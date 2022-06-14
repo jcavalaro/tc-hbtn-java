f9fa3477bc88abcb3c2fff518ec29545da00c06b
jcavalaro <jcavalaro@ciandt.com>
2022-06-14 15:22:40 -0300

    groupingBy dentro de groupingBy

---
+++ Streams_Collector_Optional_exercicios/quantidade_pessoas_por_cargo_idade/ConsultaPessoas.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
+        /*
+        nome do cargo como chave do TreeMap e um TreeSet de pessoas daquele cargo como valor do TreeMap.ok
+A lista de cargos do TreeMap deve estar em ordem reversa.
+*/
+        TreeMap<String, TreeSet<Pessoa>> treemap =
+        pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Collections.reverseOrder()), Collectors.toCollection(TreeSet::new)
+                        ));
+        return treemap;
+
+    }
+
+    public static Map<String, Long> obterContagemPessoasPorCargo(List<Pessoa> pessoas){
+        Map<String, Long> map =
+                pessoas.stream()
+                        .collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.counting()));
+
+        return map;
+    }
+
+    public static Map<String, Map<Integer, Long>> obterContagemPessoasPorCargoEIdade(List<Pessoa> pessoas){
+    	
+    	Map<String, Map<Integer, Long>> map =
+    			pessoas.stream()
+    			.collect(Collectors.groupingBy(Pessoa::getCargo, Collectors.groupingBy(Pessoa::getIdade, Collectors.counting())));
+    	
+    	return map;
+    }
+    
+}
