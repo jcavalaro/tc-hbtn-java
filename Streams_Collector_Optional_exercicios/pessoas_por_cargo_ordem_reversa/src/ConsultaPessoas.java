0a51d90fcd0dfa3116e7cf99d4aa8fe666b5ac1e
jcavalaro <jcavalaro@ciandt.com>
2022-06-14 14:10:53 -0300

    task 0 com treemap e treeset

---
+++ Streams_Collector_Optional_exercicios/pessoas_por_cargo_ordem_reversa/src/ConsultaPessoas.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.groupingBy;
+
+public class ConsultaPessoas {
+
+    public static TreeMap<String, TreeSet<Pessoa>> obterPessoasAgrupadasPorCargoEmOrdemReversa(List<Pessoa> pessoas){
+        /*
+        nome do cargo como chave do TreeMap e um TreeSet de pessoas daquele cargo como valor do TreeMap.ok
+A lista de cargos do TreeMap deve estar em ordem reversa.
+*/
+        TreeMap<String, TreeSet<Pessoa>> treemap =
+        pessoas.stream()
+                .collect(Collectors.groupingBy(Pessoa::getCargo, () -> new TreeMap<>(Collections.reverseOrder()), Collectors.toCollection(TreeSet::new)
+                        ));
+        return treemap;
+
+
+    }
+
+}
